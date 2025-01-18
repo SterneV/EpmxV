be9e6ae01533855a62d8040faa1cd93da14c0755
SterneV <Sterne.dyl@gmail.com>
2025-01-18 02:01:18 +0100

    Initial commit: Set up Fabric modding project

---
+++ src/client/kotlin/sterne/epmxv/EpmxVClient.kt
@@ -0,0 +1,9 @@
+package sterne.epmxv
+
+import net.fabricmc.api.ClientModInitializer
+
+object EpmxVClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/sterne/epmxv/EpmxVDataGenerator.kt
@@ -0,0 +1,9 @@
+package sterne.epmxv
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object EpmxVDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
